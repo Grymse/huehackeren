c4064a45ea2c020d573eba8fb6fa5a5022148bc9
Nicolai Grymer <pronicoxd@gmail.com>
2023-11-01 22:33:06 +0100

    y

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { ArduinoHueReader } from "./script";
+
+  const reader = new ArduinoHueReader(console.log);
+</script>
+
+<main>
+  <button on:click={reader.connect}> Connect </button>
+</main>
---
+++ src/script.ts
@@ -0,0 +1,85 @@
+import type SerialPort from "serialport";
+
+export class ArduinoHueReader {
+  private onData: (data: string | undefined) => void;
+  private port: SerialPort | undefined;
+
+  constructor(onData: (data: string | undefined) => void) {
+    this.onData = onData;
+  }
+
+  async connect() {
+    // Prompt user to select any serial port
+    const port = await navigator.serial.requestPort();
+
+    // Open the port with the specified baud rate
+    await port.open({ baudRate: 115200 });
+
+    // Define the text decoder
+    const textDecoder = new TextDecoderStream();
+    const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+    const reader = textDecoder.readable.getReader();
+
+    // Read loop
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) {
+        reader.releaseLock();
+        break;
+      }
+
+      this.onData(value);
+    }
+
+    await readableStreamClosed;
+    await port.close();
+  }
+}
+/* 
+
+function errMessage(err) {
+  console.log("\x1b[31m", "#####################################");
+  console.log("\x1b[31m", err);
+  console.log("\x1b[31m", "#####################################", "\x1b[0m");
+}
+function errOneLine(err) {
+  console.log("\x1b[31m", err, "\x1b[0m");
+}
+
+function errBar() {
+  console.log("\x1b[31m", "#####################################", "\x1b[0m");
+}
+
+
+function start(path) {
+  const port = new SerialPort(path, { baudRate: 115200 });
+  port.on("error", portError);
+  const parser = new Readline();
+  port.pipe(parser);
+
+  parser.on("data", (line) => {
+    if (line.substring(0, 7) === "http://") {
+      const input = line.split("|");
+      const data = JSON.parse(input[1]);
+      if (data.hue !== undefined) {
+        if (data.hue < 0) {
+          data.hue += 65536;
+        }
+      }
+      sendData(input[0], data);
+      if (!errorState) log(`> ${input[0]}::${JSON.stringify(data)}`);
+    } else {
+      console.log("\x1b[36m", `> ${line}`, "\x1b[0m");
+    }
+  });
+}
+
+function triggerErrorState() {
+  errorState = true;
+  clearTimeout(triggerErrorState.timeout);
+  triggerErrorState.timeout = setTimeout(() => {
+    errorState = false;
+  }, 2000);
+}
+
+ */
